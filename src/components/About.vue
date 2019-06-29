43ea5c5c02a6dd4e37c80b13431112673c7a1012
Sujana <spanta@unomaha.edu>
2019-06-29 17:09:39 -0500

    assignment1

---
+++ src/components/About.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="about">
+    <h1>Hello {{ their_name }}, this is an about page</h1>
+    <p>This is Sujana’s To Do list developed in Vue.Js to learn Vue.Js</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'About',
+  data() {
+    return {
+      their_name: this.$route.params.name
+    }
+  }
+}
+</script>
